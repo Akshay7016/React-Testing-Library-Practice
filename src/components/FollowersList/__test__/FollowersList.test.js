d4682b0ca6b2380a3e4ba5106d822f6d595e45b0
Akshay7016 <asajgule@gmail.com>
2024-01-01 20:46:27 +0530

    hooks like beforeEach, beforeAll, afterEach, afterAll used

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -12,6 +12,24 @@ const MockFollowersList = () => {
 };
 
 describe("FollowersList", () => {
+
+    beforeEach(() => {
+        console.log("RUNNING BEFORE EACH TEST");
+    });
+
+    beforeAll(() => {
+        console.log("RAN ONCE BEFORE ALL TESTS");
+    });
+
+    afterEach(() => {
+        console.log("RUNNING AFTER EACH TEST");
+    });
+
+    afterAll(() => {
+        console.log("RAN ONCE AFTER ALL TESTS");
+
+    })
+
     it("should fetch followers data", async () => {
         render(<MockFollowersList />);
         const followerDivElement = await screen.findByTestId("follower-item-0");
